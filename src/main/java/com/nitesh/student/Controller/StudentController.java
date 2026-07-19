f965f05f3222c0b7485de4d3311bce9fe2896cce
Nitesh Pokhrel <niteshpokhrel84@gmail.com>
2026-07-19 09:32:55 +0545

    Implemented update in student

--- src/main/java/com/nitesh/student/Controller/StudentController.java
+++ src/main/java/com/nitesh/student/Controller/StudentController.java
@@ -33,4 +33,19 @@ public class StudentController {
         studentService.deleteById(id);
         return true;
     }
+    @PutMapping("id/{id}")
+    public StudentEntity updateStudentById(@PathVariable ObjectId id, @RequestBody StudentEntity newEntry) {
+        StudentEntity old = studentService.findById(id).orElse(null);
+        if (old != null) {
+            old.setAge(newEntry.getAge() != null ? newEntry.getAge() : old.getAge());
+            old.setGrade(newEntry.getGrade() != null && !newEntry.getGrade().equals("") ? newEntry.getGrade() : old.getGrade());
+            old.setName(newEntry.getName() != null && !newEntry.getName().equals("") ? newEntry.getName() : old.getName());
+            old.setContact_no(newEntry.getContact_no() != null ? newEntry.getContact_no() : old.getContact_no());
+            old.setParents_name(newEntry.getParents_name() != null && !newEntry.getParents_name().equals("") ? newEntry.getParents_name() : old.getParents_name());
+            studentService.saveStudent(old);
+            return old;
+        } else {
+            return null;
+        }
+    }
 }
