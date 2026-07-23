5af0a20fbba47b8425ff4eea58291e3b0be63c9e
Nitesh Pokhrel <niteshpokhrel84@gmail.com>
2026-07-23 22:33:30 +0545

    implement ResponseEntity in Student controller

--- src/main/java/com/nitesh/student/Controller/StudentController.java
+++ src/main/java/com/nitesh/student/Controller/StudentController.java
@@ -4,9 +4,12 @@ import com.nitesh.student.Entity.StudentEntity;
 import com.nitesh.student.Services.StudentServices;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/student_data")
@@ -16,29 +19,42 @@ public class StudentController {
 
 
     @PostMapping
-    public StudentEntity saveStudent(@RequestBody StudentEntity studentEntity){
-        studentService.saveStudent(studentEntity);
-        return studentEntity;
+    public ResponseEntity<StudentEntity> saveStudent(@RequestBody StudentEntity studentEntity){
+       try {
+           studentService.saveStudent(studentEntity);
+           return new ResponseEntity<>(HttpStatus.CREATED);
+       }
+       catch (Exception e)
+       {
+           return  new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+       }
+
     }
 
     @GetMapping
-   public List <StudentEntity> getAll(){
-      return  studentService.getAll();
+   public ResponseEntity<List<StudentEntity>> getAll(){
+
+        return  new ResponseEntity<>(studentService.getAll(), HttpStatus.OK);
     }
 
-    @GetMapping("id/{id}")
-    public StudentEntity findById( @PathVariable ObjectId id){
-        return studentService.findById(id).orElse(null);
+    @GetMapping("/id/{id}")
+    public ResponseEntity<StudentEntity> findById( @PathVariable ObjectId id){
+       Optional<StudentEntity> student = studentService.findById(id);
+        if(student.isPresent()){
+            return new ResponseEntity<>(studentService.findById(id).orElse(null) , HttpStatus.OK);
+        }
+       else{
+           return  new ResponseEntity<>(HttpStatus.NOT_FOUND);        }
     }
 
-    @DeleteMapping("id/{id}")
-    public boolean deleteById(@PathVariable ObjectId id){
-        studentService.deleteById(id);
-        return true;
+    @DeleteMapping("/id/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable ObjectId id){
+       studentService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PutMapping("id/{id}")
-    public StudentEntity updateStudentById(@PathVariable ObjectId id, @RequestBody StudentEntity newEntry) {
+    @PutMapping("/id/{id}")
+    public ResponseEntity<?> updateStudentById(@PathVariable ObjectId id, @RequestBody StudentEntity newEntry) {
         StudentEntity old = studentService.findById(id).orElse(null);
         if (old != null) {
             old.setAge(newEntry.getAge() != null ? newEntry.getAge() : old.getAge());
@@ -47,9 +63,9 @@ public class StudentController {
             old.setContact_no(newEntry.getContact_no() != null ? newEntry.getContact_no() : old.getContact_no());
             old.setParents_name(newEntry.getParents_name() != null && !newEntry.getParents_name().equals("") ? newEntry.getParents_name() : old.getParents_name());
             studentService.saveStudent(old);
-            return old;
+            return new ResponseEntity<>(HttpStatus.OK);
         } else {
-            return null;
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 }
