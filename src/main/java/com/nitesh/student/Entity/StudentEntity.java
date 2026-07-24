4995c4b6509b2760cc9d20ccbbbac3e676aae9ef
Nitesh Pokhrel <niteshpokhrel84@gmail.com>
2026-07-24 08:27:19 +0545

    Implement Project Lombook in student and teacher entity

--- src/main/java/com/nitesh/student/Entity/StudentEntity.java
+++ src/main/java/com/nitesh/student/Entity/StudentEntity.java
@@ -1,10 +1,12 @@
 package com.nitesh.student.Entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document (collection = "student_entry")
+@Data
 public class StudentEntity {
 
     @Id
@@ -14,55 +16,4 @@ public class StudentEntity {
     private String grade;
     private String Parents_name;
     private Long contact_no;
-
-    public StudentEntity() {
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public Long getAge() {
-        return age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public void setAge(Long age) {
-        this.age = age;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setGrade(String grade) {
-        this.grade = grade;
-    }
-
-    public String getGrade() {
-        return grade;
-    }
-
-    public void setContact_no(Long contact_no) {
-        this.contact_no = contact_no;
-    }
-
-    public Long getContact_no() {
-        return contact_no;
-    }
-
-    public String getParents_name() {
-        return Parents_name;
-    }
-
-    public void setParents_name(String parents_name) {
-        Parents_name = parents_name;
-    }
 }
